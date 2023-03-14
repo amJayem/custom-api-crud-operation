2518dc4e7d601a74ee2742f1af6bc8653d472603
amJayem <csejayem19@gmail.com>
2023-03-14 22:04:48 +0600

    delete api is created, others modified

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { ObjectId } = require("mongodb");
 const router = express.Router();
 const userModel = require("../models/model");
 
@@ -6,32 +7,37 @@ router.use(express.json());
 
 // getting all data from users collection
 router.get("/users", async (req, res) => {
-  const result = await userModel.find();
-  res.status(201).send(`
-  <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Age</th>
-          <th>Email</th>
-        </tr>
-      </thead>
-      <tbody>
-     ${result.map(
-       (user) =>
-         `<tr>
-         <td>${user.name}</td>
-         <td>${user.age}</td>
-         <td>${user.email}</td>
-       </tr>`
-     )} 
-      </tbody>
-    </table>
+  try {
+    const result = await userModel.find();
+    const table = (`
+    <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+      ${result.map(
+        (user) =>
+          `<tr>
+          <td>${user.name}</td>
+          <td>${user.age}</td>
+          <td>${user.email}</td>
+        </tr>`
+      )} 
+        </tbody>
+      </table>
     `);
+    res.status(201).send(table)
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
 });
 
 // storing user data to users collection
-router.post("/user", async (req, res) => {
+router.post("/add-user", async (req, res) => {
   try {
     const { name, age, email } = req.body;
     const newData = new userModel({ name, age, email });
@@ -55,6 +61,17 @@ router.put("/update-user/:id", async (req, res) => {
       new: true,
     });
     res.status(201).send(updatedUser);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+router.delete("/delete-user/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    // const filter = { _id: ObjectId(id) };
+    const result = await userModel.findByIdAndDelete(id);
+    res.status(201).send(result);
   } catch (error) {
     res.status(500).send({ message: error.message });
   }
