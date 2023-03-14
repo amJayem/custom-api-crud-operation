7e97a66ea2cca6b8b9c5aeefc2de036bb4f20849
amJayem <csejayem19@gmail.com>
2023-03-14 15:13:12 +0600

    route imported and interact with api

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const db = require('./config/dbConnect');
+const routes = require('./routes/routes')
 
 const app = express();
 const port = 8000;
 
+app.use('/api',routes);
 
 app.listen(port, () => {
   console.log(`Server started on port http://localhost:${port}`);
