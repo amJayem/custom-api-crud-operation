460a24a9abef059830b8c575c8ba668136c67815
amJayem <csejayem19@gmail.com>
2023-03-14 16:13:59 +0600

    user info update api created

--- routes/routes.js
+++ routes/routes.js
@@ -9,8 +9,10 @@ router.get("/users", async (req, res) => {
   const result = await userModel.find();
   res.status(201).send(result);
   // data is consoled when this route will be hit
-  console.table({result});
-  result.map(r=>console.log("name: ",[r.name],"email: ",[r.email],"age: ",[r.age]))
+  console.table({ result });
+  result.map((r) =>
+    console.log("name: ", [r.name], "email: ", [r.email], "age: ", [r.age])
+  );
 });
 
 // storing user data to users collection
@@ -20,6 +22,25 @@ router.post("/user", async (req, res) => {
     const newData = new userModel({ name, age, email });
     const result = await newData.save();
     res.status(201).send(result);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+// user info can be updated by id
+router.put("/update-user/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userInfo = {
+      name: req.body.name,
+      age: req.body.age,
+      email: req.body.email,
+    };
+    const updatedUser = await userModel.findByIdAndUpdate(id, userInfo, {
+      new: true,
+    });
+    res.status(201).send(updatedUser);
+
   } catch (error) {
     res.status(500).send({ message: error.message });
   }
