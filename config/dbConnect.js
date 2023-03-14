54c573eadb47ec1ced3a67668fe4e03173a8ef0f
amJayem <csejayem19@gmail.com>
2023-03-14 15:09:14 +0600

    database name added to the link

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.42e2srw.mongodb.net/?retryWrites=true&w=majority`,
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.42e2srw.mongodb.net/DevsNest?retryWrites=true&w=majority`,
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => console.log("MongoDB Connected"))
