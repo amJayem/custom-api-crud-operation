3ac44586babf65de1820383919424f3169354376
amJayem <csejayem19@gmail.com>
2023-03-14 12:55:19 +0600

    schema created, other files modified

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pl2ayam.mongodb.net/?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB Connected'))
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.42e2srw.mongodb.net/?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const db = require('./config/dbConnect');
 
 const app = express();
-const port = 3000;
+const port = 8000;
 
 
 app.listen(port, () => {
